4696d6e93a1186511d0c19f5a609e482c3043046
sapt3 <saptarshi.ironmaiden@gmail.com>
2017-01-01 20:55:06 +0530

    Documented and minor changes

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-var Hapi = require('hapi');
-var mqtt = require('mqtt');
+var Hapi = require('hapi'); //Fetch the modules required to set up the server
+var mqtt = require('mqtt'); //Fetch the modules required to establish the MQTT protocol
 
-var server = new Hapi.Server();
-var port = Number(process.env.PORT || 4444);
+var server = new Hapi.Server(); //Set up a new Server
+var port = Number(process.env.PORT || 4444); //Define the port no. NOTE: The default is 4444 for local servers otherwiser it is set by default from Heroku
 
-server.connection(
-  { port: port,
-    routes: { cors: true }
+//Set up the connection and define the variables
+server.connection({
+   port,
+   routes: {
+      cors: true }
   });
 
-var client  = mqtt.connect('mqtt://test.mosquitto.org:1883');
+var client  = mqtt.connect('mqtt://test.mosquitto.org:1883'); //Set up a client which connects to the broker i.e. test.mosquitto.org on port 1883
 
-var mqttPublish = function(topic, msg){
-  client.publish(topic, msg, function() {
-    console.log('msg sent: ' + msg);
+//function declaraation
+var mqttPublish = (topic, msg) => {
+  client.publish(topic, msg, () => {
+    console.log('Message sent: ' + msg);
   });
 }
 
@@ -21,14 +24,16 @@ server.route([
   {
     method: 'POST',
     path: '/device/control',
-    handler: function (request, reply) {
-      var deviceInfo = 'dev' + request.payload.deviceNum + '-' + request.payload.command;
+    handler: (request, reply) => {
+      var deviceInfo = 'dev' + request.payload.deviceNum + '-' + request.payload.command; //eg - 'dev1-on'
       reply(deviceInfo);
-      mqttPublish('device/control', deviceInfo, {
+      mqttPublish('device/control', deviceInfo, { //Publish the device info ot the topic which the ESP is subscribed to.
         'qos' : 2
       });
     }
   }
 ]);
 
-server.start();
+server.start(() => {
+  console.log(`Server set up on port ${port}`);
+});
