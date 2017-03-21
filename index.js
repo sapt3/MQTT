0aa2e4e645db15cf211840045dc8ecb41983c656
sapt3 <saptarshi.ironmaiden@gmail.com>
2017-03-21 11:05:29 +0530

    changed back to test.mosquitto.org

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.connection({
    { cors: true }
   });
 
-var client  = mqtt.connect('mqtt://hc.iandwe.in'); //Set up a client which connects to the broker i.e. test.mosquitto.org on port 1883
+var client  = mqtt.connect('mqtt://test.mosquitto.org:1883'); //Set up a client which connects to the broker i.e. test.mosquitto.org on port 1883
 
 //NOTE: Function declaration for publish to broker
 var mqttPublish = (topic, msg) => {
